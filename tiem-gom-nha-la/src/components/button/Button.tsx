2cc1c7655d908be5eb4c4f9ef0d1e3061ad23dad
hyun12 <huypq1801@gmail.com>
2024-04-11 13:41:17 +0700

    config docker

---
+++ tiem-gom-nha-la/src/components/button/Button.tsx
@@ -0,0 +1,12 @@
+import { FloatButton } from 'antd'
+import ButtonGroup from 'antd/es/button/button-group'
+
+function Button() {
+  return (
+    <ButtonGroup>
+      <FloatButton />
+    </ButtonGroup>
+  )
+}
+
+export default Button
---
+++ tiem-gom-nha-la/src/pages/login_signup/Auth.tsx
@@ -0,0 +1,5 @@
+function LoginAndSignUp() {
+  return <div>LoginAndSignUp</div>
+}
+
+export default LoginAndSignUp
